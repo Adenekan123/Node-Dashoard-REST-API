d2cf16e0e43bb87c8202f62a977e126f0d70e27a
Adenekan Usman <mr.nekan123@gmail.com>
2023-03-26 15:44:04 +0100

    cors issue

--- index.js
+++ index.js
@@ -47,13 +47,7 @@ var whitelist = [
   "http://localhost:3000",
 ];
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: whitelist,
   methods: ["POST", "PUT", "GET", "PATCH", "OPTIONS", "HEAD", "DELETE"],
   credentials: true,
 };
