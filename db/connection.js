1a4c176173d0ead8bec67d41265d35ae9071d2c1
Adenekan Usman <mr.nekan123@gmail.com>
2023-03-26 10:31:11 +0100

    Added mongoDB cluster setup

--- db/connection.js
+++ db/connection.js
@@ -16,7 +16,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("here:", process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/dashboard"
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
