eb4a39fec309293a5ba0edf5d784dc350ca19147
Adenekan123 <mr.nekan123@gmail.com>
2023-04-15 13:06:18 +0100

    updated register router

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,7 +22,7 @@ const register = async function (req, res) {
       res.status(200).json(token);
     }
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(409).json({ message: err.message });
   }
 };
 
